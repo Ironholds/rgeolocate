533b5d07b5aa7e9628505b2494812c00c84d4709
ironholds <ironholds@gmail.com>
2015-05-17 18:34:07 -0400

    explicitly expect JSON

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,4 @@
 query <- function(url){
   result <- GET(url, user_agent("rgeolocate - https://github.com/Ironholds/rgeolocate"))
-  return(content(result))
+  return(content(result, as = "parsed", type = "application/json"))
 }
