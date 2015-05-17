ab317962c6a7f6e6f647d8c17cc15e81aa93c279
ironholds <ironholds@gmail.com>
2015-05-17 18:44:14 -0400

    add db_ip

---
+++ R/db_ip.R
@@ -0,0 +1,33 @@
+db_ip_query <- function(ip, key){
+  results <- query(paste0("http://api.db-ip.com/addrinfo?addr=", ip,
+                          "&api_key=", key))
+  return(results)
+}
+
+#'@title Geolocate IP Addresses Through db-ip.com
+#'@description \code{ip_api} consumes a vector of IP addresses
+#'and geolocates them via \href{https://db-ip.com/}{db-ip.com}.
+#'See `Details` for more information.
+#'
+#'@param ip_addresses a character vector of IP addresses.
+#'
+#'@param key a db-ip.com API key.
+#'
+#'@details \href{http://db-ip.com/}{db-ip.com} provides IP geolocation,
+#'either for free (with a cap of 2,500 requests a day) or in various paid tiers.
+#'
+#'To use the service, register there for an API key, and plug that key and the
+#'IP addresses into \code{\link{db_ip}}
+#'
+#'@return a list of lists, each containing the geolocated values for one IP address.
+#'The variables found depend on the level of access your API key has; see the
+#'\href{http://db-ip.com/api/#key}{API documentation} for more information.
+#'
+#'@examples
+#'\dontrun{
+#'db_ip(ip_addresses = "173.194.67.1", key = "ThisIsNotARealKey")
+#'}
+#'@export
+db_ip <- function(ip_addresses, key){
+  lapply(ip_addresses, db_ip_query, key = key)
+}
