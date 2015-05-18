f323f33a5c268397ac3c4bed97cb8e0655fd5e1b
ironholds <ironholds@gmail.com>
2015-05-18 11:46:33 -0400

    Fix #2

--- R/ip_api.R
+++ R/ip_api.R
@@ -1,5 +1,7 @@
-query_ip_api <- function(ip){
-  Sys.sleep(0.10)
+query_ip_api <- function(ip, sleep){
+  if(sleep){
+    Sys.sleep(0.10)
+  }
   result <- query(paste0("http://ip-api.com/json/", ip))
   if(result$status == "fail"){
     return(paste("Request failed:", result$message))
@@ -16,6 +18,12 @@ query_ip_api <- function(ip){
 #'
 #'@param as_data_frame whether to return the results as a data.frame or not.
 #'Set to TRUE by default.
+#'
+#'@param delay whether or not to delay each request by 100ms. ip-api.com has a
+#'maximum threshold of 2,500 requests a minute; if you're parallelising calls, you
+#'might run into this. \code{delay} allows you to set a delay between requests, taking
+#'advantage of parallelisation while avoiding running into this threshold. Set to
+#'FALSE by default
 #'
 #'@return either a data.frame or a list of vectors. If an IP cannot be geolocated, it
 #'will provide an error message: see the examples for field names and examples of each
@@ -36,8 +44,8 @@ query_ip_api <- function(ip){
 #'result <- ip_api("argh", as_data_frame = FALSE)
 #'}
 #'@export
-ip_api <- function(ip_addresses, as_data_frame = TRUE){
-  results <- lapply(ip_addresses, query_ip_api)
+ip_api <- function(ip_addresses, as_data_frame = TRUE, delay = FALSE){
+  results <- lapply(ip_addresses, query_ip_api, delay)
   if(as_data_frame){
     return(ip_to_df(results))
   }
