fc60ffb47abddb611bd68f7123d6f789cf2f9bdd
Oliver Keyes <ironholds@gmail.com>
2016-10-19 16:26:10 -0700

    Fix #32

--- tests/testthat/test_ip2location.R
+++ tests/testthat/test_ip2location.R
@@ -1,5 +1,5 @@
 context("Test ip2location")
-file <- system.file("extdata","ip2_4_sample.bin", package = "rgeolocate")
+file <- system.file("extdata","ip2_sample.bin", package = "rgeolocate")
 test_ips <- c("66.249.69.245","2A04:0000:0000:0000:0000:0000:0000:0000")
 
 test_that("non-existent files are detected", {
@@ -7,7 +7,7 @@ test_that("non-existent files are detected", {
 })
 
 test_that("non-existent IPs are detected", {
-  results <- ip2location("foo", file, "country_name")
+  results <- ip2location("foo", file, TRUE, "country_name")
   expect_true(is.na(results[1,1]))
 })
 
@@ -16,4 +16,14 @@ test_that("IPv4 and 6 geolocation works with large-field files", {
   expect_identical(results$country_name, c("United States", "Germany"))
 })
 
-test_that("Lat/long retrieval works")
+test_that("Lat/long retrieval works", {
+  result <- ip2location(test_ips, file, TRUE, c("lat", "long"))
+  expect_true(is.na(result$lat[1]))
+  expect_gt(result$lat[2], 50)
+  expect_lt(result$lat[2], 51)
+  expect_true(is.na(result$long[1]))
+  expect_gt(result$long[2], 8)
+  expect_lt(result$long[2], 9)
+  
+  
+})
