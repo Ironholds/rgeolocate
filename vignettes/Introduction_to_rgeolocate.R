f1ebbbecb030586005f79b18f02fcf04c8959ec0
Os Keyes <ironholds@gmail.com>
2021-10-20 11:11:52 -0700

    sigh

--- vignettes/Introduction_to_rgeolocate.R
+++ vignettes/Introduction_to_rgeolocate.R
@@ -1,4 +1,4 @@
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(rgeolocate)
 file <- system.file("extdata","GeoLite2-Country.mmdb", package = "rgeolocate")
 results <- maxmind("196.200.60.51", file, c("continent_name", "country_code", "country_name"))
